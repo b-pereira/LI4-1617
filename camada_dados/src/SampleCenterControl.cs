b0c6b500a8e2926ef9ddb20a9c6f3f4ac55d4070
Bruno Pereira <pereirabruno05@gmail.com>
2017-06-15 00:11:02 +0100

    Add tests to CenterControl

--- camada_dados/src/SampleCenterControl.cs
+++ camada_dados/src/SampleCenterControl.cs
@@ -2,27 +2,33 @@ using System;
 public class SampleCenterControl {
 	[STAThread]
 	static void Main(string[] args) {
-        //CreateBasedeDadosMMDatabaseSchema.Main(args);
-        //CreateBasedeDadosMMData.Main(args);
-        //RetrieveAndUpdateBasedeDadosMMData.Main(args);
-        //ListBasedeDadosMMData.Main(args);
-        //DeleteBasedeDadosMMData.Main(args);
-        //DropBasedeDadosMMDatabaseSchema.Main(args);
+        try
+        {
+            //CreateBasedeDadosMMDatabaseSchema.Main(args);
+            //CreateBasedeDadosMMData.Main(args);
+            //RetrieveAndUpdateBasedeDadosMMData.Main(args);
+            //ListBasedeDadosMMData.Main(args);
+            //DeleteBasedeDadosMMData.Main(args);
+            //DropBasedeDadosMMDatabaseSchema.Main(args);
 
-        Console.WriteLine("A iniciar ... aguarde!");
+            Console.WriteLine("A iniciar ... aguarde!");
 
-        AreaCliente cl = new AreaCliente(1);
+            AreaCliente cl = new AreaCliente(1);
 
-        //cl.RegistarCliente("fljaljfla", "mm", "José Lopes da Silva");
+            //cl.RegistarCliente("fljaljfla", "mm", "José Lopes da Silva");
+            cl.GerarPedido("Beef");
+            //cl.EscolherIguaria(2, 1);
+            //cl.EscolherIguaria(1, 1);
+            //cl.ListarTendencias();
 
-        //cl.EscolherIguaria(2, 1);
-        //cl.EscolherIguaria(1, 1);
-        cl.ListarTendencias();
+            Console.ReadLine();
+        }
+        finally
+        {
+            BasedeDadosMMPersistentManager.Instance().DisposePersistentManager();
+        }
 
-        Console.ReadLine();
-
-        
-	}
+    }
 	
 }
 
