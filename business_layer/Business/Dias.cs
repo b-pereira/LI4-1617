26110fa2b792ba8590e082fbb89dc20173c59c51
Bruno Pereira <pereirabruno05@gmail.com>
2017-06-15 19:27:41 +0100

    Add enum types for weekday and menu dish status for crud

---
+++ business_layer/Business/Dias.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business
+{
+    public enum Dias : byte
+    {
+        Domingo = 1,
+        SegundaFeira = 2,
+        TercaFeira = 3,
+        QuartaFeira = 4,
+        QuintaFeira = 5,
+        SextaFeira = 6,
+        Sabado = 7
+    }
+}
